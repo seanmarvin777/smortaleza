43558e61d181d7174360ffbc4b301b245dd2b7c0
Sean Marvin Ortaleza <ortalezaseanmarvin@gmail.com>
2026-02-27 15:26:20 +0800

    Updated html, css and JS of jci-silang website on projects

--- projects/jci-silang/scripts.js
+++ projects/jci-silang/scripts.js
@@ -2,33 +2,40 @@ const slides = document.querySelectorAll('.photo-slideshow .slide');
 let currentSlide = 0;
 
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.remove('active');
-        if(i === index) slide.classList.add('active');
-    });
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) slide.classList.add('active');
+  });
 }
 
-// Auto slideshow every 3 seconds
-setInterval(() => {
+// Auto slideshow every 3 seconds (only if slides exist)
+if (slides.length > 0) {
+  showSlide(0);
+  setInterval(() => {
     currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
-}, 3000);
+  }, 3000);
+}
 
 // Simple slider for about-photo-slider
 const aboutSlides = document.querySelectorAll('.about-photo-slider .about-slide');
 let aboutCurrent = 0;
 
 function showAboutSlide(idx) {
-    aboutSlides.forEach((slide, i) => {
-        slide.classList.remove('active');
-        if (i === idx) slide.classList.add('active');
-    });
+  aboutSlides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === idx) slide.classList.add('active');
+  });
 }
 
-setInterval(() => {
+// Auto slider for About page (only if slides exist)
+if (aboutSlides.length > 0) {
+  showAboutSlide(0);
+  setInterval(() => {
     aboutCurrent = (aboutCurrent + 1) % aboutSlides.length;
     showAboutSlide(aboutCurrent);
-}, 3000);
+  }, 3000);
+}
 
 
 // Responsive platinum-container image activation
